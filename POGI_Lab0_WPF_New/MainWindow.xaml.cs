af888cda44fb5889f2b0250b0f3e971550ad5d33
Plohoyprogrammist <plohoyprogrammist@gmail.com>
2026-09-10 10:45:19 +0700

    Всё работает, можно сливать

--- POGI_Lab0_WPF_New/MainWindow.xaml.cs
+++ POGI_Lab0_WPF_New/MainWindow.xaml.cs
@@ -27,6 +27,7 @@ namespace POGI_Lab0_WPF_New
             Point2D p2 = new Point2D(rnd.Next(0, (int)Scene.Width), rnd.Next(0, (int)Scene.Height));
             Point2D p3 = new Point2D(rnd.Next(0, (int)Scene.Width), rnd.Next(0, (int)Scene.Height));
             tr = new Triangle(p1, p2, p3);
+            DrawTriangle(tr);
         }
 
         public void DrawLine(Point2D p1, Point2D p2)
