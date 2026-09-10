e90e4b193b30c6851300c5ebc64488c67f848e5e
Plohoyprogrammist <plohoyprogrammist@gmail.com>
2026-09-10 11:08:16 +0700

    Коммит Для сохранения

--- POGI_Lab0_WPF_New/Rectangle.cs
+++ POGI_Lab0_WPF_New/Rectangle.cs
@@ -4,6 +4,20 @@ public class Rectangle
 {
 	public Rectangle()
 	{
+		public Point2D P1 {  get; private set; }
+		public Point2D P2 {  get; private set; }
+		public Point2D P3 {  get; private set; }
+		public Point2D P4 {  get; private set; }
+
+		public Rectangle(Point2D p1,  Point2D p2, Point2D p3,  Point2D p4)
+		{
+			P1 = p1;
+			P2 = p2;
+			P3 = p3;
+			P4 = p4;
+		}
+		
+		public AddX(int x) {
 
 	}
 }
