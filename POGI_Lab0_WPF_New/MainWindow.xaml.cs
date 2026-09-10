8c2c00a1082ac2aad1db5b56ae05c2892de46518
Plohoyprogrammist <plohoyprogrammist@gmail.com>
2026-09-10 10:42:36 +0700

    Добаввил рисовашку треугольника

--- POGI_Lab0_WPF_New/MainWindow.xaml.cs
+++ POGI_Lab0_WPF_New/MainWindow.xaml.cs
@@ -16,9 +16,17 @@ namespace POGI_Lab0_WPF_New
     /// </summary>
     public partial class MainWindow : Window
     {
+        Triangle tr;
+        Random rnd = new Random();
+
         public MainWindow()
         {
             InitializeComponent();
+            //Создание треугольника со случайными координатами
+            Point2D p1 = new Point2D(rnd.Next(0, (int)Scene.Width), rnd.Next(0, (int)Scene.Height));
+            Point2D p2 = new Point2D(rnd.Next(0, (int)Scene.Width), rnd.Next(0, (int)Scene.Height));
+            Point2D p3 = new Point2D(rnd.Next(0, (int)Scene.Width), rnd.Next(0, (int)Scene.Height));
+            tr = new Triangle(p1, p2, p3);
         }
 
         public void DrawLine(Point2D p1, Point2D p2)
@@ -36,5 +44,14 @@ namespace POGI_Lab0_WPF_New
             //Добавление линии в Canvas
             Scene.Children.Add(line);
         }
+
+        public void DrawTriangle(Triangle tr)
+        {
+            DrawLine(tr.P1, tr.P2);
+            DrawLine(tr.P2, tr.P3);
+            DrawLine(tr.P3, tr.P1);
+        }
+
+
     }
 }
